ef0760fde2cc7e35b39bfb33212cfff013068508
Michele Balistreri <michele@briksoftware.com>
2015-01-17 12:16:01 +0100

    use Ecto timestamps

--- lib/crudex/model.ex
+++ lib/crudex/model.ex
@@ -18,7 +18,7 @@ defmodule Crudex.Model do
     quote do
       @primary_key {:id, Crudex.JSONUUID, []}
       @foreign_key_type Crudex.JSONUUID
-      @timestamp_type Crudex.JSONDateTime
+      @timestamps_type Crudex.JSONDateTime
       schema unquote(schema_name) do
         unquote(block)
         timestamps inserted_at: :created_at
